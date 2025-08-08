46cd11cfa91771d4b24c8af13d50a18a6c8be145
Abhi123-coder-art <tundare.abhijeet@gmail.com>
2025-08-08 21:03:33 +0530

    For_Each_Loop

---
+++ Day_three/src/com/Day_4/For_Each_Loop.java
@@ -0,0 +1,16 @@
+package com.Day_4;
+
+public class For_Each_Loop {
+
+	public static void main(String[] args) {
+		
+		
+		int a[]= {10,20,30,40,50,60};
+		
+		for (int i:a) {
+			System.out.println(i +" ");
+		}
+
+	}
+
+}
