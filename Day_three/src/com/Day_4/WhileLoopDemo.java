d7609ea9cb8346c674f2cbd55cd23131574d6b67
Abhi123-coder-art <tundare.abhijeet@gmail.com>
2025-08-08 20:51:24 +0530

    While loop Demo

---
+++ Day_three/src/com/Day_4/WhileLoopDemo.java
@@ -0,0 +1,15 @@
+package com.Day_4;
+
+public class WhileLoopDemo {
+
+	public static void main(String[] args) {
+		int userinput =91;
+		while(userinput<=100)
+		{
+			System.out.println(userinput);
+			userinput++;
+		}
+
+	}
+
+}
