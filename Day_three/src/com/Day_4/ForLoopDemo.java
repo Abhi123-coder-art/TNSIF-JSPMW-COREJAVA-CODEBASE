8537fbf6e8dde2735176c3ea66b863171d5e9350
Abhi123-coder-art <tundare.abhijeet@gmail.com>
2025-08-08 20:43:26 +0530

    For loop demo

---
+++ Day_three/src/com/Day_4/ForLoopDemo.java
@@ -0,0 +1,14 @@
+package com.Day_4;
+
+public class ForLoopDemo {
+
+	public static void main(String[] args) {
+		for (int i =1; i<=100; i++)
+		{
+			System.out.println("Value of i :" +i);
+		}
+			
+
+	}
+
+}
