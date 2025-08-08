0ffbca29231c42fe98567b2aad83c08805e3abe1
Abhi123-coder-art <tundare.abhijeet@gmail.com>
2025-08-08 20:58:35 +0530

    Do_While_Loop

---
+++ Day_three/src/com/Day_4/Do_While_Loop_Demo.java
@@ -0,0 +1,16 @@
+package com.Day_4;
+
+public class Do_While_Loop_Demo {
+
+	public static void main(String[] args) {
+		int i = 11;
+		do {
+			System.out.println(i);
+			i++;
+			
+		}
+		while(i<=20);
+
+	}
+
+}
