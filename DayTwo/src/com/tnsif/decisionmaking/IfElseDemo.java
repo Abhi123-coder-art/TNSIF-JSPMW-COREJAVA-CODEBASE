12cef9b811f732a78e904779b55464a0d6dcaeab
Abhi123-coder-art <tundare.abhijeet@gmail.com>
2025-09-13 16:10:02 +0530

    Day Two

---
+++ DayTwo/src/com/tnsif/decisionmaking/IfElseDemo.java
@@ -0,0 +1,21 @@
+package com.tnsif.decisionmaking;
+
+public class IfElseDemo {
+
+	public static void main(String[] args) {
+		
+		
+        int age =21;
+		
+		if(age >= 18)
+		{
+			System.out.println("You are eligible for voting");
+			
+		}
+		else {
+			System.out.println("Sorry !  You have to wait till 18");
+		}
+
+	}
+
+}
