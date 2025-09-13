2920448db94f06f4e134e4235e43ba15c49f229e
Abhi123-coder-art <tundare.abhijeet@gmail.com>
2025-09-13 15:33:32 +0530

    Day One

---
+++ DayOne/src/com/tnsif/dayone/CharDemo.java
@@ -0,0 +1,19 @@
+package com.tnsif.dayone;
+
+public class CharDemo {
+
+	public static void main(String[] args) {
+		
+		
+		char ch='a';
+		System.out.println(ch);
+		
+		
+		char ch1=65;
+		System.out.println(ch1);
+		
+		int a='A';
+
+	}
+
+}
---
+++ DayOne/src/com/tnsif/dayone/IdentifierDemo.java
@@ -0,0 +1,20 @@
+package com.tnsif.dayone;
+
+public class IdentifierDemo {
+
+	public static void main(String[] args) {
+		
+		
+        //Datatype    varname = data
+		int $num=12;
+		System.out.println("Value of the number varible is :"+ $num);
+		String studentName="Abhijeet";
+		System.out.println("Value of name is :"+studentName );
+		
+		int age;
+		
+		age=20;
+
+	}
+
+}
